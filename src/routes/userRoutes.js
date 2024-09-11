34096208fb995e3ff7569ef054f90ed2caf085e2
eduardo <edunegreiross@gmail.com>
2024-09-11 10:11:39 -0300

    Atualização do codigo

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,15 @@ import userController from '../controllers/UserController';
 
 const router = new Router();
 
-router.get('/', userController.index);
+router.get('/', userController.store);
 
 export default router;
+
+/*
+Métodos para controller
+index => para listar os usuários => GET
+store/create => cria um novo usuário -> POST
+delete => apaga um usuário -> DELETE
+show => mostra um usuário -> GET
+update => atualiza um usuário -> PATCH OU PUT
+*/
