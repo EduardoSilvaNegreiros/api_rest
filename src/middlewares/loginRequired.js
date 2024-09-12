77c45c011346b78ef5dda4154b49092f46ca3a88
eduardo <edunegreiross@gmail.com>
2024-09-12 10:36:21 -0300

    Criação do middleware loginRequired

---
+++ src/middlewares/loginRequired.js
@@ -0,0 +1,9 @@
+export default (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      errors: ['Precisa fazer login'],
+    });
+  }
+};
