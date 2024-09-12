0e3e168971e4354d6640cd48688dba830d9a1107
eduardo <edunegreiross@gmail.com>
2024-09-12 09:17:08 -0300

    Criacção do Delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -53,11 +53,38 @@ class UserController {
 
       return res.json(novosDados);
     } catch (e) {
-      return res.json(null);
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
     }
   }
 
   // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          erros: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          erros: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new UserController();
