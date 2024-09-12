606e6e51e0f45f4139732c7b071ca674f4826852
eduardo <edunegreiross@gmail.com>
2024-09-12 10:42:28 -0300

    Atualização loginRequired

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,9 +1,24 @@
+import jwt from 'jsonwebtoken';
+
 export default (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
     return res.status(401).json({
       errors: ['Precisa fazer login'],
+    });
+  }
+
+  const [texto, token] = authorization.split(' ');
+
+  try {
+    const dados = jwt.verify(token, process.env.TOKEN_SECRET);
+    const { id, email } = dados;
+    req.user.id = id;
+    req.user.Email = email;
+  } catch (e) {
+    return res.status(401).json({
+      erros: ['Token expirado ou inválido'],
     });
   }
 };
