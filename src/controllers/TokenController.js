f1af8c09203b0b739e8a2b092883896008f27286
eduardo <edunegreiross@gmail.com>
2024-09-12 09:50:15 -0300

    Atualização TokenController

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -2,6 +2,16 @@ import User from '../models/User';
 
 class TokenController {
   async store(req, res) {
+    const { email = '', password = '' } = req.body;
+
+    if (!email || !password) {
+      return res.status(401).json({
+        errors: ['Credenciais Inválidas'],
+      });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
     res.json('OK');
   }
 }
