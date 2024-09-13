bf2fd315f573a0bcc6489bc77d4a68def1a0a932
eduardo <edunegreiross@gmail.com>
2024-09-13 16:33:25 -0300

    Atualização do HomeController.js

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,15 +1,20 @@
-import Aluno from '../models/Aluno';
+import Aluno from '../models/Aluno'; // Importa o modelo 'Aluno', que representa uma tabela no banco de dados.
 
 class HomeController {
+  // Método assíncrono 'index', responsável por criar um novo aluno e retornar os dados criados.
   async index(req, res) {
+    // Cria um novo aluno no banco de dados usando o método 'create' do Sequelize.
+    // Os dados do aluno são passados diretamente como um objeto.
     const novoAluno = await Aluno.create({
-      nome: 'Eduardo',
-      sobrenome: 'Negreiros',
-      email: 'edunegreiross@gmail.com',
-      idade: 18,
-      peso: 85,
-      altura: 1.7,
+      nome: 'Eduardo', // Nome do aluno.
+      sobrenome: 'Negreiros', // Sobrenome do aluno.
+      email: 'edunegreiross@gmail.com', // Email do aluno.
+      idade: 18, // Idade do aluno.
+      peso: 85, // Peso do aluno em quilogramas.
+      altura: 1.7, // Altura do aluno em metros.
     });
+
+    // Retorna os dados do aluno recém-criado em formato JSON como resposta à requisição.
     res.json(novoAluno);
   }
 }
