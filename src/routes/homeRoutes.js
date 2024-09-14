2c3d43c17136a66b97d761f8fc4bcae82b88a6b1
eduardo <edunegreiross@gmail.com>
2024-09-14 10:28:41 -0300

    Atualização no homeRoutes.js

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,8 +1,14 @@
+// Importa o módulo Router do Express para criar um roteador
 import { Router } from 'express';
+
+// Importa o controlador responsável pelas operações da rota inicial
 import homeController from '../controllers/HomeController';
 
+// Cria uma nova instância do roteador
 const router = new Router();
 
+// Define a rota GET para a URL base ('/'), associando-a ao método 'index' do HomeController
 router.get('/', homeController.index);
 
+// Exporta o roteador configurado para ser utilizado em outras partes da aplicação
 export default router;
