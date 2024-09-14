46e0afb1f770572188e045b78697921bb1fd63a9
eduardo <edunegreiross@gmail.com>
2024-09-14 10:29:55 -0300

    Atualização no index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,36 +1,42 @@
+// Importa o módulo Sequelize para gerenciar a conexão com o banco de dados
 import { Sequelize } from 'sequelize';
+
+// Importa a configuração do banco de dados
 import databaseConfig from '../config/database';
+
+// Importa os modelos que serão usados na aplicação
 import Aluno from '../models/Aluno';
 import User from '../models/User';
 
+// Cria uma lista de modelos para serem inicializados com a conexão
 const models = [Aluno, User];
 
+// Cria uma nova instância de Sequelize para gerenciar a conexão com o banco de dados
 const connection = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
+  process.env.DATABASE_NAME, // Nome do banco de dados
+  process.env.DATABASE_USERNAME, // Nome de usuário do banco de dados
+  process.env.DATABASE_PASSWORD, // Senha do banco de dados
 
   {
-    dialect: 'mysql',
-    host: process.env.DATABASE_HOST,
-    port: 3306,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    dialect: 'mysql', // Dialeto do banco de dados (MySQL)
+    host: process.env.DATABASE_HOST, // Endereço do host do banco de dados
+    port: 3306, // Porta padrão do MySQL
+    username: process.env.DATABASE_USERNAME, // Nome de usuário do banco de dados
+    password: process.env.DATABASE_PASSWORD, // Senha do banco de dados
+    database: process.env.DATABASE_NAME, // Nome do banco de dados
     define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
+      timestamps: true, // Habilita os timestamps automáticos (created_at e updated_at)
+      underscored: true, // Usa nomes de colunas com sublinhados em vez de camelCase
+      underscoredAll: true, // Usa sublinhados para todos os nomes de colunas e tabelas
+      createdAt: 'created_at', // Define o nome da coluna de criação
+      updatedAt: 'updated_at', // Define o nome da coluna de atualização
     },
-
     dialectOptions: {
-      timezone: 'America/Sao_Paulo',
+      timezone: 'America/Sao_Paulo', // Define o fuso horário para o banco de dados
     },
-
-    timezone: 'America/Sao_Paulo',
+    timezone: 'America/Sao_Paulo', // Define o fuso horário para a aplicação
   },
 );
 
+// Inicializa todos os modelos com a conexão do Sequelize
 models.forEach((model) => model.init(connection));
