f74a5f2b4b1a4bf68519d9e83d8be3cb3e652e03
eduardo <edunegreiross@gmail.com>
2024-09-14 10:29:05 -0300

    Atualização no tokenRoutes.js

--- src/routes/tokenRoutes.js
+++ src/routes/tokenRoutes.js
@@ -1,8 +1,14 @@
+// Importa o módulo Router do Express para criar um roteador
 import { Router } from 'express';
+
+// Importa o controlador responsável pelas operações relacionadas a tokens
 import tokenController from '../controllers/TokenController';
 
+// Cria uma nova instância do roteador
 const router = new Router();
 
+// Define a rota POST para a URL base ('/'), associando-a ao método 'store' do TokenController
 router.post('/', tokenController.store);
 
+// Exporta o roteador configurado para ser utilizado em outras partes da aplicação
 export default router;
