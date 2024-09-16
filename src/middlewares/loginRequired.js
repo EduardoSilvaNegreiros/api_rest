a2fb97d2724df8cb540544889ea058b477331a88
eduardo <edunegreiross@gmail.com>
2024-09-16 11:15:54 -0300

    Tentativa de debug novamente

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -3,6 +3,7 @@ import User from '../models/User';
 
 export default async (req, res, next) => {
   const { authorization } = req.headers; // Extrai o cabeçalho de autorização da requisição.
+  console.log('Token:', authorization);
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res.status(401).json({
@@ -16,13 +17,20 @@ export default async (req, res, next) => {
     // Verifica o token JWT usando a chave secreta armazenada em 'process.env.TOKEN_SECRET'.
     // Se o token for válido, extrai os dados (id e email) contidos no token.
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
+    console.log('Dados do token:', dados);
 
     const userId = dados.id.id || dados.id; // Verifica se id é um objeto ou um valor simples
     const userEmail = dados.email;
 
-    const user = await User.findOne({ where: { id: userId, email: userEmail } });
+    const user = await User.find({
+      where: {
+        id: userId,
+        email: userEmail,
+      },
+    });
 
     if (!user) {
+      console.log('Erro ao buscar usuário:', userId, userEmail);
       return res.status(401).json({
         errors: ['Usuário Inválido'],
       });
@@ -31,9 +39,7 @@ export default async (req, res, next) => {
     // Chama o próximo middleware da cadeia.
     return next();
   } catch (e) {
-    console.error('Erro ao verificar token:', e);
-    console.error('Erro mensagem:', e.message);
-    console.error('Erro stack:', e.stack); // Se houver qualquer erro durante a verificação do token
+    console.log('Erro ao verificar token:', e);
     return res.status(401).json({
       errors: ['Token expirado ou inválido.'],
     });
