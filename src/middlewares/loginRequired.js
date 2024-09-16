62a2a0d95faf85282861e4f2580fef0608d83db8
eduardo <edunegreiross@gmail.com>
2024-09-16 10:48:03 -0300

    Tentativa de resolução de bugs

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -13,19 +13,14 @@ export default async (req, res, next) => {
 
   // O cabeçalho de autorização geralmente tem o formato "Bearer token".
   // Aqui, ele divide o valor em duas partes e armazena a segunda (o token) na variável 'token'.
-  const [, token] = authorization.split(' ');
+  const token = authorization.trim();
 
   try {
     // Verifica o token JWT usando a chave secreta armazenada em 'process.env.TOKEN_SECRET'.
     // Se o token for válido, extrai os dados (id e email) contidos no token.
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
-    const { id, email } = dados; // Extrai o valor numérico do id
-    const user = await User.findOne({
-      where: {
-        id,
-        email,
-      },
-    });
+
+    const user = await User.findOne({ where: { id: dados.id.id, email: dados.email } });
 
     if (!user) {
       return res.status(401).json({
