5233795876a7128d6712e019e9d573b7067c35ad
eduardo <edunegreiross@gmail.com>
2024-09-16 12:16:28 -0300

    Tentativa de debug novamente

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -3,7 +3,6 @@ import User from '../models/User';
 
 export default async (req, res, next) => {
   const { authorization } = req.headers; // Extrai o cabeçalho de autorização da requisição.
-  console.log('Token:', authorization);
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res.status(401).json({
@@ -12,17 +11,17 @@ export default async (req, res, next) => {
   }
 
   const token = authorization.split(' ')[1];
+  console.log(User); // Verifique se o modelo User está disponível
 
   try {
     // Verifica o token JWT usando a chave secreta armazenada em 'process.env.TOKEN_SECRET'.
     // Se o token for válido, extrai os dados (id e email) contidos no token.
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
-    console.log('Dados do token:', dados);
-
-    const userId = dados.id.id || dados.id; // Verifica se id é um objeto ou um valor simples
+    console.log('Decoded token:', dados);
+    const userId = dados.id.id;
     const userEmail = dados.email;
 
-    const user = await User.find({
+    const user = await User.findOne({
       where: {
         id: userId,
         email: userEmail,
