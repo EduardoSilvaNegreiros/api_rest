ee3601eb7601630e8dcb07b084d8d3e1109c5762
eduardo <edunegreiross@gmail.com>
2024-09-16 10:57:24 -0300

    Tentativa de debug em loginRequired

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -4,23 +4,23 @@ import User from '../models/User';
 export default async (req, res, next) => {
   const { authorization } = req.headers; // Extrai o cabeçalho de autorização da requisição.
 
-  // Verifica se o cabeçalho de autorização está presente. Se não estiver, retorna erro 401.
-  if (!authorization) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     return res.status(401).json({
       errors: ['Login requerido'],
     });
   }
 
-  // O cabeçalho de autorização geralmente tem o formato "Bearer token".
-  // Aqui, ele divide o valor em duas partes e armazena a segunda (o token) na variável 'token'.
-  const token = authorization.trim();
+  const token = authorization.split(' ')[1];
 
   try {
     // Verifica o token JWT usando a chave secreta armazenada em 'process.env.TOKEN_SECRET'.
     // Se o token for válido, extrai os dados (id e email) contidos no token.
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
 
-    const user = await User.findOne({ where: { id: dados.id.id, email: dados.email } });
+    const userId = dados.id.id || dados.id; // Verifica se id é um objeto ou um valor simples
+    const userEmail = dados.email;
+
+    const user = await User.findOne({ where: { id: userId, email: userEmail } });
 
     if (!user) {
       return res.status(401).json({
@@ -31,7 +31,9 @@ export default async (req, res, next) => {
     // Chama o próximo middleware da cadeia.
     return next();
   } catch (e) {
-    // Se houver qualquer erro durante a verificação do token
+    console.error('Erro ao verificar token:', e);
+    console.error('Erro mensagem:', e.message);
+    console.error('Erro stack:', e.stack); // Se houver qualquer erro durante a verificação do token
     return res.status(401).json({
       errors: ['Token expirado ou inválido.'],
     });
