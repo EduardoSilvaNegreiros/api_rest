9bc0d4b6cc3008e24aa4d2423ca575255e473f29
eduardo <edunegreiross@gmail.com>
2024-09-17 11:15:13 -0300

    Atualização do loginRequires.js

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,68 +1,40 @@
-import jwt from 'jsonwebtoken'; // Importa o módulo JWT (JSON Web Token) para gerar e verificar tokens.
+import jwt from 'jsonwebtoken';
 import User from '../models/User';
 
-const verifyToken = (token) => {
+export default async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      errors: ['Login required'],
+    });
+  }
+
+  const [, token] = authorization.split(' ');
+
   try {
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
-    return dados;
-  } catch (e) {
-    console.log('Erro ao verificar token:', e);
-    return null;
-  }
-};
+    const { id, email } = dados;
 
-export default async (req, res, next) => {
-  const { authorization } = req.headers; // Extrai o cabeçalho de autorização da requisição.
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).json({
-      errors: ['Login requerido'],
-    });
-  }
-
-  const token = authorization.split(' ')[1];
-  let dados;
-  try {
-    dados = jwt.verify(token, process.env.TOKEN_SECRET);
-    console.log('Token verificado com sucesso:', dados);
-  } catch (e) {
-    console.error('Erro ao verificar token:', e);
-    return res.status(401).json({
-      errors: ['Token expirado ou inválido.'],
-    });
-  }
-
-  const userId = dados.id.id;
-  const userEmail = dados.email;
-
-  try {
     const user = await User.findOne({
       where: {
-        id: userId,
+        id,
+        email,
       },
     });
 
     if (!user) {
-      console.error('Usuário não encontrado no banco de dados:', userId, userEmail);
       return res.status(401).json({
-        errors: ['Usuário Inválido'],
+        errors: ['Usuário inválido'],
       });
     }
 
-    console.log('Usuário encontrado com sucesso:', user);
-  } catch (e) {
-    console.error('Erro ao buscar usuário:', e);
-    return res.status(500).json({
-      errors: ['Erro interno do servidor'],
-    });
-  }
-
-  try {
+    req.userId = id;
+    req.userEmail = email;
     return next();
   } catch (e) {
-    console.error('Erro ao chamar próximo middleware:', e);
-    return res.status(500).json({
-      errors: ['Erro interno do servidor'],
+    return res.status(401).json({
+      errors: ['Token expirado ou inválido.'],
     });
   }
 };
