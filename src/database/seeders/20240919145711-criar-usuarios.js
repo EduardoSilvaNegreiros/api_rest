0b6ba35286a356e78fd9f6ea3e59b3605896e7ba
eduardo <edunegreiross@gmail.com>
2024-09-19 12:14:21 -0300

    Atualização seeders

--- src/database/seeders/20240919145711-criar-usuarios.js
+++ src/database/seeders/20240919145711-criar-usuarios.js
@@ -1,33 +1,31 @@
 const bcryptjs = require('bcryptjs');
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-
+  async up(queryInterface) {
     await queryInterface.bulkInsert('users', [
       {
-      name: 'Luiz',
-      email: 'luiz1@gmail.com',
-      password_hash: await bcryptjs.hash('123456', 8),
-      created_at: new Date(),
-      updated_at: new Date(),
-    },
+        nome: 'Luiz',
+        email: 'luiz1@gmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
       {
-      name: 'Eduardo',
-      email: 'edu@gmail.com',
-      password_hash: await bcryptjs.hash('1234567', 8),
-      created_at: new Date(),
-      updated_at: new Date(),
-    },
+        nome: 'Eduardo',
+        email: 'edu@gmail.com',
+        password_hash: await bcryptjs.hash('1234567', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
       {
-      name: 'Geronimo',
-      email: 'geronimo@gmail.com',
-      password_hash: await bcryptjs.hash('12345678', 8),
-      created_at: new Date(),
-      updated_at: new Date(),
-    },
+        nome: 'Geronimo',
+        email: 'geronimo@gmail.com',
+        password_hash: await bcryptjs.hash('12345678', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
     ],
-      {});
+    {});
   },
 
-  down() => { },
 };
