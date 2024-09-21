2bd9e6c35154347fd2527998383a261bda1125be
eduardo <edunegreiross@gmail.com>
2024-09-20 23:36:59 -0300

    Comentação do código

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,7 +1,10 @@
 class HomeController {
+  // Método assíncrono 'index' que lida com a requisição GET na rota '/'
   async index(req, res) {
+    // Envia uma resposta em formato JSON com a string 'Index'
     res.json('Index');
   }
 }
 
+// Exporta uma instância da classe HomeController
 export default new HomeController();
