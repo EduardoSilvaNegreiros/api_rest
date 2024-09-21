06012ed736adbe65832df8f19804a0691e625a24
eduardo <edunegreiross@gmail.com>
2024-09-20 23:40:42 -0300

    Comentação do código

--- src/routes/tokenRoutes.js
+++ src/routes/tokenRoutes.js
@@ -1,8 +1,14 @@
+// Importa o Router do Express para criar rotas
 import { Router } from 'express';
+
+// Importa o controlador responsável pela autenticação de tokens
 import tokenController from '../controllers/TokenController';
 
+// Cria uma nova instância do Router
 const router = new Router();
 
+// Define uma rota POST para a raiz ('/') que chama o método store do TokenController
 router.post('/', tokenController.store);
 
+// Exporta o router para que possa ser utilizado em outras partes da aplicação
 export default router;
