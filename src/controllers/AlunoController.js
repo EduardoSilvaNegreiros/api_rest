28c61261a1eaf5da4677f775ed3bd52333fc7b65
eduardo <edunegreiross@gmail.com>
2024-09-20 23:35:29 -0300

    Comentação do código

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,10 +1,16 @@
+// Importa o modelo Aluno, que representa a tabela 'alunos' no banco de dados
 import Aluno from '../models/Aluno';
 
 class AlunoController {
+  // Método assíncrono index - responsável por listar todos os alunos
   async index(req, res) {
+    // Busca todos os registros da tabela 'alunos' no banco de dados
     const alunos = await Aluno.findAll();
+
+    // Retorna a lista de alunos em formato JSON como resposta
     res.json(alunos);
   }
 }
 
+// Exporta uma instância da classe AlunoController
 export default new AlunoController();
