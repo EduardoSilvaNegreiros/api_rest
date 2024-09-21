51d6f181c81d498a4bbb8270409f2ec30613bf2b
eduardo <edunegreiross@gmail.com>
2024-09-20 23:41:45 -0300

    Comentação do código

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,8 +1,14 @@
+// Importa o Router do Express para criar rotas
 import { Router } from 'express';
+
+// Importa o controlador responsável pelas operações relacionadas a alunos
 import alunoController from '../controllers/AlunoController';
 
+// Cria uma nova instância do Router
 const router = new Router();
 
+// Define uma rota GET para a raiz ('/') que chama o método index do AlunoController
 router.get('/', alunoController.index);
 
+// Exporta o router para que possa ser utilizado em outras partes da aplicação
 export default router;
