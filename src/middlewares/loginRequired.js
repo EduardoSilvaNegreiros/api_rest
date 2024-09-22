c7d82bf53bd5c1744f5886f76d8fdc58c327dce9
eduardo <edunegreiross@gmail.com>
2024-09-21 23:43:56 -0300

    Comentação de código

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -2,20 +2,27 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User';
 
 export default async (req, res, next) => {
+  // Extrai o campo authorization dos cabeçalhos da requisição
   const { authorization } = req.headers;
 
+  // Verifica se o campo authorization está presente
   if (!authorization) {
     return res.status(401).json({
-      errors: ['Login requerido'],
+      errors: ['Login requerido'], // Retorna erro se o token não for encontrado
     });
   }
 
+  // Divide a string no espaço e pega a segunda parte (o token em si)
   const [, token] = authorization.split(' ');
 
   try {
+    // Verifica e decodifica o token usando a chave secreta definida nas variáveis de ambiente
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
+
+    // Extrai o id e o email dos dados decodificados do token
     const { id, email } = dados;
 
+    // Busca o usuário no banco de dados com o id e email obtidos do token
     const user = await User.findOne({
       where: {
         id,
@@ -23,6 +30,7 @@ export default async (req, res, next) => {
       },
     });
 
+    // Se o usuário não for encontrado, retorna um erro
     if (!user) {
       return res.status(401).json({
         errors: ['Usuário inválido'],
@@ -31,8 +39,11 @@ export default async (req, res, next) => {
 
     req.userId = id;
     req.userEmail = email;
+
+    // Chama o próximo middleware ou controlador
     return next();
   } catch (e) {
+    // Se ocorrer um erro durante a verificação do token, retorna um erro de autenticação
     return res.status(401).json({
       errors: ['Token expirado ou inválido.'],
     });
