8faa4ff01f9f22739d9fb18ca8d191d46a88ccc8
eduardo <edunegreiross@gmail.com>
2024-09-26 11:55:26 -0300

    Precisão de login no alunoRoutes

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -4,15 +4,17 @@ import { Router } from 'express';
 // Importa o controlador responsável pelas operações relacionadas a alunos
 import alunoController from '../controllers/AlunoController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 // Cria uma nova instância do Router
 const router = new Router();
 
 // Define uma rota GET para a raiz ('/') que chama o método index do AlunoController
 router.get('/', alunoController.index);
-router.post('/', alunoController.store);
-router.put('/:id', alunoController.update);
+router.post('/', loginRequired, alunoController.store);
+router.put('/:id', loginRequired, alunoController.update);
 router.get('/:id', alunoController.show);
-router.delete('/:id', alunoController.delete);
+router.delete('/:id', loginRequired, alunoController.delete);
 
 // Exporta o router para que possa ser utilizado em outras partes da aplicação
 export default router;
