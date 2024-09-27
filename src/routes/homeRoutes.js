eecea6ddef4132729583783aba682e8b3cacef32
eduardo <edunegreiross@gmail.com>
2024-09-27 19:49:57 -0300

    Atualização homeRoutes.js

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,15 +1,14 @@
-// Importa o Router do Express para gerenciar rotas
+// Importa o Router do Express
 import { Router } from 'express';
 
-// Importa o controlador da Home, que contém a lógica para a rota inicial
+// Importa o controlador da Home
 import homeController from '../controllers/HomeController';
 
-// Cria uma nova instância do Router para definir as rotas da aplicação
+// Cria uma nova instância do Router
 const router = new Router();
 
-// Define a rota GET para a página inicial ('/')
-// Quando o usuário acessar a rota '/', o método 'index' do 'homeController' será executado
-router.get('/', homeController.index);
+// Define a rota GET para a página inicial
+router.get('/', homeController.index); // Executa 'index' do 'homeController'
 
-// Exporta o router para que ele possa ser utilizado em outros arquivos
+// Exporta o router
 export default router;
