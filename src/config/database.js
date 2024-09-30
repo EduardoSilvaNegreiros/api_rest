ce1566d9f17d309de5d5d074070bb04c294f63ce
eduardo <edunegreiross@gmail.com>
2024-09-29 21:14:22 -0300

    Atualização database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,30 +1,25 @@
-// Carrega as variáveis de ambiente do arquivo .env
 require('dotenv').config();
 
 module.exports = {
-  // Define o tipo de banco de dados que será usado (MySQL neste caso)
-  dialect: 'mysql',
+  dialect: 'mysql', // Tipo de banco de dados
 
-  // Configurações de conexão com o banco de dados
-  host: process.env.DATABASE_HOST, // Endereço do servidor de banco de dados
-  port: process.env.DATABASE_PORT, // Porta do servidor de banco de dados
-  username: process.env.DATABASE_USERNAME, // Nome de usuário para autenticação
-  password: process.env.DATABASE_PASSWORD, // Senha do usuário
-  database: process.env.DATABASE, // Nome do banco de dados a ser acessado
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
 
-  // Configurações específicas do dialecto (MySQL)
   dialectOptions: {
-    timezone: 'America/Sao_Paulo', // Define o fuso horário para o banco de dados
+    timezone: 'America/Sao_Paulo',
   },
 
-  timezone: 'America/Sao_Paulo', // Define o fuso horário da aplicação
+  timezone: 'America/Sao_Paulo',
 
-  // Configurações padrão para novos modelos criados
   define: {
-    timestamps: true, // Adiciona automaticamente os campos createdAt e updatedAt
-    underscored: true, // Utiliza o estilo snake_case para os campos no banco de dados
-    underscoredAll: true, // Aplica o estilo snake_case a todos os nomes de campos
-    createdAt: 'created_at', // Altera o nome do campo createdAt para created_at
-    updatedAt: 'updated_at', // Altera o nome do campo updatedAt para updated_at
+    timestamps: true, // Campos de data automática
+    underscored: true, // Estilo snake_case
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
   },
 };
