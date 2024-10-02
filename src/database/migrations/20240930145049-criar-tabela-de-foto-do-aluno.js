30f77775a7e1b6fde28d5bef0b1c232e5eb3ecfc
eduardo <edunegreiross@gmail.com>
2024-10-02 10:08:06 -0300

    Atualização na migration sobre foto do aluno

--- src/database/migrations/20240930145049-criar-tabela-de-foto-do-aluno.js
+++ src/database/migrations/20240930145049-criar-tabela-de-foto-do-aluno.js
@@ -1,22 +1,33 @@
-'use strict';
+require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('fotos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('fotos');
+  },
 };
