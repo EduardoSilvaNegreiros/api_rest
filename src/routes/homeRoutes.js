ae45a44109d8ddf904a5d222e5b143490b3a375e
eduardo <edunegreiross@gmail.com>
2024-10-04 15:44:51 -0300

    Organização do homeRoutes.js

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,14 +1,9 @@
-// Importa o Router do Express
 import { Router } from 'express';
-
-// Importa o controlador da Home
 import homeController from '../controllers/HomeController';
 
-// Cria uma nova instância do Router
 const router = new Router();
 
-// Define a rota GET para a página inicial
-router.get('/', homeController.index); // Executa 'index' do 'homeController'
+// Rota para a página inicial
+router.get('/', homeController.index);
 
-// Exporta o router
 export default router;
