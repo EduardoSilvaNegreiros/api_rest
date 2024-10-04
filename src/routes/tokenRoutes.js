32c8fd8064e7ec032747b7230745959eaac653b9
eduardo <edunegreiross@gmail.com>
2024-10-04 15:43:05 -0300

    Organização do tokenRoutes.js

--- src/routes/tokenRoutes.js
+++ src/routes/tokenRoutes.js
@@ -1,14 +1,9 @@
-// Importa o Router do Express para criar rotas
 import { Router } from 'express';
-
-// Importa o controlador responsável pela autenticação de tokens
 import tokenController from '../controllers/TokenController';
 
-// Cria uma nova instância do Router
 const router = new Router();
 
-// Define uma rota POST para a raiz ('/') que chama o método store do TokenController
+// Rota para gerar tokens
 router.post('/', tokenController.store);
 
-// Exporta o router para que possa ser utilizado em outras partes da aplicação
 export default router;
