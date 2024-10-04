73d1aa5650b78f0aa1009c16cf79da27f1b299f7
eduardo <edunegreiross@gmail.com>
2024-10-04 15:41:44 -0300

    Comentação de fotoRoutes.js

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import loginRequired from '../middlewares/loginRequired';
-
 import fotoController from '../controllers/FotoController';
 
 const router = new Router();
 
+// Rota para upload de fotos, com autenticação obrigatória
 router.post('/', loginRequired, fotoController.store);
 
 export default router;
