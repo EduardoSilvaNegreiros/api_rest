c7a6d5172bcc8417211c6cadb3a7c6852fc59e2a
eduardo <edunegreiross@gmail.com>
2024-10-27 22:53:31 -0300

    Explicando o código

--- src/app.js
+++ src/app.js
@@ -2,23 +2,25 @@
 import './database';
 import { resolve } from 'path';
 
-// Importa o framework Express para criar o servidor e gerenciar rotas
+// Importa o framework Express e bibliotecas de segurança
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 
-// Importa as rotas definidas em arquivos separados
+// Importa as rotas
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
 
+// Define a lista de origens permitidas para CORS
 const whitelist = [
   'https://react1.eduardonegreiros.com.br',
   'http://localhost:3000',
 ];
 
+// Configurações de CORS
 const corsOptions = {
   origin(origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
@@ -29,17 +31,14 @@ const corsOptions = {
   },
 };
 
+// Classe principal da aplicação
 class App {
   constructor() {
-    // Cria uma instância da aplicação Express
     this.app = express();
-
-    // Configura middlewares e rotas
     this.middlewares();
     this.routes();
   }
 
-  // Método que configura os middlewares
   middlewares() {
     this.app.use(cors(corsOptions));
     this.app.use(helmet());
@@ -48,18 +47,14 @@ class App {
     this.app.use(express.static(resolve(__dirname, '..', 'uploads', 'images')));
   }
 
-  // Método que define as rotas da aplicação
   routes() {
     this.app.use('/', homeRoutes);
-
     this.app.use('/users/', userRoutes);
-
     this.app.use('/tokens/', tokenRoutes);
-
     this.app.use('/alunos/', alunoRoutes);
     this.app.use('/fotos/', fotoRoutes);
   }
 }
 
-// Exporta a instância da aplicação Express para ser utilizada no servidor
+// Exporta a instância da aplicação
 export default new App().app;
