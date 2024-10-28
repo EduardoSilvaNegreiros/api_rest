61b0dff4570381b8622732d42db6679db090c397
eduardo <edunegreiross@gmail.com>
2024-10-27 22:51:54 -0300

    Voltando a config anterior de token

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -32,7 +32,7 @@ class TokenController {
     });
 
     // Retorna o token e informações do usuário
-    return res.json({ token, user: { nome: user.nome, id, email } });
+    return res.json({ token, user: { nome, id, email } });
   }
 }
 
