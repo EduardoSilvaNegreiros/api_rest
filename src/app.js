7d2084f3ccb8cc9217be87f24a11683b5068bab8
eduardo <edunegreiross@gmail.com>
2024-10-27 22:48:47 -0300

    Adicionando verificação de CORS para permitir apenas origens da whitelist

--- src/app.js
+++ src/app.js
@@ -20,8 +20,12 @@ const whitelist = [
 ];
 
 const corsOptions = {
-  origin(origin, callbacks) {
-    
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
   },
 };
 
